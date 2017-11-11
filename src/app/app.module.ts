28478d2167891d30e87a51c3fb4f5f142832b0fe
Manuel <m.a.c.p90@gmail.com>
2017-11-11 14:31:50 -0500

    shit working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes,RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactServiceService } from './contact-service.service';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  { path: 'contact/:id', component: ContactComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ContactServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ContactComponent implements OnInit {
+  contactId: number;
+  optionalParameter: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe((params) => {
+        this.contactId = +params['id'];
+    });
+
+    this.route.queryParams
+      .subscribe((queryParams) => {
+        this.optionalParameter = queryParams['foo'];
+    });
+  }
+}
